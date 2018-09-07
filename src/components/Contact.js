b7dc1a74d19255c6c151931d768a0d120754f99a
Daniel-Cross <crossy1686@gmail.com>
2018-09-07 14:59:22 +0100

    added portfolio and contact submit button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,19 +4,42 @@ import '../styles/Contact.css';
 class Contact extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
   }
 
   render() {
     return (
       <div className="Contact">
         <div className="contact-title">Get in touch</div>
-        <form className="contact-form">
-          <input className="contact-input" type="text" placeholder="Name" />
+        <form className="contact-form" onSubmit={this.handleSubmit}>
+          <input
+            className="contact-input"
+            type="text"
+            placeholder="Name"
+            name="name"
+            onChange={this.handleChange}
+          />
           <input
             className="contact-input"
             type="email"
             placeholder="Email Address"
+            name="email"
+            onChange={this.handleChange}
           />
           <textarea
             rows="8"
@@ -24,8 +47,13 @@ class Contact extends Component {
             className="contact-input"
             type="text"
             placeholder="Your message"
+            name="message"
+            onChange={this.handleChange}
           />
         </form>
+        <button className="contact-button">
+          <i className="far fa-envelope" />
+        </button>
       </div>
     );
   }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../styles/Portfolio.css';
 
 class Portfolio extends Component {
   constructor(props) {
@@ -10,15 +11,73 @@ class Portfolio extends Component {
   render() {
     return (
       <div className="Portfolio">
-        <div className="category-tabs">
-          <div
-            activeTab={this.state.activeTab}
-            onChange={tabID => this.setState({ activeTab: tabID })}
-          >
-            <div>React App</div>
-            <div>React App</div>
-            <div>React App</div>
-            <div>React App</div>
+        <div className="card">
+          <img
+            className="avatar"
+            src={require('/Users/DanielCross/Projects/portfolio/src/images/Screen Shot 2018-09-05 at 12.56.12.png')}
+            alt="Avatar"
+          />
+          <div className="container">
+            <h4>
+              <b>Surreal Estate</b>
+            </h4>
+            <p>
+              Property search engine similar to Right Move or Zoopla. Built
+              using React
+            </p>
+          </div>
+          <div className="card-buttons">
+            <a
+              href="https://github.com/Daniel-Cross/surreal-estate"
+              target="_blank"
+            >
+              <button className="work-button">GitHub</button>
+            </a>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            className="avatar"
+            src={require('/Users/DanielCross/Projects/portfolio/src/images/Screen Shot 2018-09-07 at 13.26.04.png')}
+            alt="Avatar"
+          />
+          <div className="container">
+            <h4>
+              <b>Weather App</b>
+            </h4>
+            <p>
+              Dynamic weather app that gives weekly weather reports based on
+              api. Built using React
+            </p>
+          </div>
+          <div className="card-buttons">
+            <a
+              href="https://github.com/Daniel-Cross/react-weather-app"
+              target="_blank"
+            >
+              <button className="work-button">GitHub</button>
+            </a>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            className="avatar"
+            src={require('/Users/DanielCross/Projects/portfolio/src/images/Screen Shot 2018-09-07 at 13.32.07.png')}
+            alt="Avatar"
+          />
+          <div className="container">
+            <h4>
+              <b>Cruise Ship</b>
+            </h4>
+            <p>Operational cruise ship with gui. Built using JavaScript</p>
+          </div>
+          <div className="card-buttons">
+            <a
+              href="https://github.com/Daniel-Cross/cruise-ships"
+              target="_blank"
+            >
+              <button className="work-button">GitHub</button>
+            </a>
           </div>
         </div>
       </div>
